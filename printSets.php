0c7040591bd5e9da13c919052610e9c629f354bc
Ashraf <amoham39@its.jnj.com>
2016-10-31 11:34:32 -0400

    second pass

--- printSets.php
+++ printSets.php
@@ -1,27 +1,16 @@
 <?php
 
-class PrintSets {
 
-	public $start, $end, $target, $set_size;
 
-	public function printResultSet($start, $end, $target, $set_size) {
-		$this->start = $start;$this->end = $end;$this->target = $target;$this->set_size = $set_size;
-	}
-
-	public function printAllCombinations() {
-		$callTimes = array();
-		if ($this->end > $this->target) {
-			if ($this->set_size == 2) {
-				for ($i=$this->start;$i<$this->end;$i++) {
+	function printAllCombinations($start, $end, $target, $set_size) {
+			if ($set_size == 2) {
+				for ($i=$start;$i<$end;$i++) {
 					echo $i . "-";
-					echo $this->target-$i  . "\n";
-				}
-			} 			
+					echo $target-$i  . "\n";
+				}		
 		}
 	}
-}
 
 
-$print = new PrintSets();
-$print->printResultSet(1,10,10,2);
-$print->printAllCombinations();
+
+printAllCombinations(1,50,70,2);
