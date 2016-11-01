0b0f36aa5356e6504385719f6d0ef793cdd05d8d
Ashraf <amoham39@its.jnj.com>
2016-11-01 07:55:29 -0400

    final pass

--- printSets.php
+++ printSets.php
@@ -1,16 +1,43 @@
 <?php
 
-
-
-	function printAllCombinations($start, $end, $target, $set_size) {
-			if ($set_size == 2) {
-				for ($i=$start;$i<$end;$i++) {
-					echo $i . "-";
-					echo $target-$i  . "\n";
-				}		
+function rec($initSet, $sum, $index, $inputArray, $target, $set_size) {
+	$output = array();
+	if ($sum == $target) {
+		if (count($initSet) == $set_size) {
+			echo "[\"";
+			for ($i=0;$i<count($initSet);$i++) {
+				echo $initSet[$i];
+				if ($i != count($initSet)-1) {
+					echo ",";
+				}
+			}
+			echo "\"]";
+		}	
+	}
+	if ($sum > $target || $index == count($inputArray) || count($initSet) >= $set_size) {
+		return;
+	}
+	$total = $target/$inputArray[$index];
+	for($i=0;$i<$total;$i++) {
+		for ($k=0;$k<$i;$k++) {
+			array_push($initSet, $inputArray[$index]);
+		}
+		rec($initSet, $sum + $i*$inputArray[$index], $index+1, $inputArray, $target, $set_size);
+		for($x=0;$x<$i;$x++) {
+			array_pop($initSet);
 		}
 	}
+}
+
+function partitionList($start, $endLimit, $target, $set_size) {
+	$numSet = array();
+	for ($i=$start;$i<$endLimit;$i++) {
+		array_push($numSet, $i);
+	}
+	$initSet = array();
+	rec($initSet, 0, 0, $numSet, $target, $set_size);
+}
 
 
-
-printAllCombinations(1,50,70,2);
+partitionList(1,70,50,2);
+partitionList(1,50,70,3);
